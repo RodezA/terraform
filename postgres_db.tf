c2d5efb345f5b694c1297a0a75b82f8b69b36ce9
Anthony Rodez <44179945+RodezA@users.noreply.github.com>
2023-03-12 15:03:41 -0600

    Add files via upload

---
+++ postgres_db.tf
@@ -0,0 +1,15 @@
+
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+provider "postgresql" {
+  host            = "postgres_server_ip"
+  port            = 5432
+  database        = "postgres"
+  username        = "postgres_user"
+  password        = "postgres_password"
+  sslmode         = "require"
+  connect_timeout = 15
+}
